e5426bc0761232475d125db3dd5707bc6482009d
Osama Qarem <osamaqarem@gmail.com>
2020-03-08 17:51:55 +0800

    update

--- Images.js
+++ Images.js
@@ -1,11 +1,27 @@
-import netflix from "./assets/netflix.png";
-import bf1 from "./assets/bf1.png";
-import airbnb from "./assets/airbnb.png";
-import jsx from "./assets/jsx.png";
-import twitch from "./assets/twitch.png";
-import message from "./assets/message.png";
-import mvc from "./assets/mvc.png";
-import flux from "./assets/flux.png";
-import vdom from "./assets/vdom.png";
+import netflix from "./assets/netflix.png"
+import bf1 from "./assets/bf1.png"
+import airbnb from "./assets/airbnb.png"
+import jsx from "./assets/jsx.png"
+import twitch from "./assets/twitch.png"
+import message from "./assets/message.png"
+import mvc_basic from "./assets/mvc_basic.png"
+import mvc from "./assets/mvc.png"
+import flux from "./assets/flux.png"
+import vdom from "./assets/vdom.png"
+import unsafebiker from "./assets/unsafebiker.png"
+import safebiker from "./assets/safebiker.jpg"
 
-export default { netflix, bf1, airbnb, twitch, jsx, message, mvc, flux, vdom };
+export default {
+  netflix,
+  bf1,
+  airbnb,
+  twitch,
+  jsx,
+  message,
+  mvc_basic,
+  mvc,
+  flux,
+  vdom,
+  unsafebiker,
+  safebiker
+}
